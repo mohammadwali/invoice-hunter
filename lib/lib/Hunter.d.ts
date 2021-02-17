e61d81b7c0c1db827e6f7184b9c4a531b3051882
Mohammad Wali <mohdwali@live.com>
2021-02-18 00:35:35 +0100

    Initial commit

--- lib/lib/Hunter.d.ts
+++
@@ -1,9 +0,0 @@
-import puppeteer from "puppeteer";
-export declare class Hunter {
-    protected steps: any;
-    protected browser: puppeteer.Browser;
-    constructor(browser: puppeteer.Browser);
-    run(): Promise<void>;
-    huntEndesa(): Promise<void>;
-    huntAgua(): Promise<void>;
-}
--- lib/models/console-message.d.ts
+++
@@ -1,9 +0,0 @@
-export declare enum ConsoleMessage {
-    TITLE = "invoice-hunter",
-    BANNER = "CLI to extract utility invoices and upload to the google drive folder",
-    ERROR = "ERROR: ",
-    SUCCESS = "SUCCESS: ",
-    INFO = "INFO: ",
-    WARNING = "WARNING: ",
-    HUNTING = "Hunting"
-}
